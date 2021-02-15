ffa797a3143a878814741453c3f3cfeaa6bd6b64
Moin Khan <kmoin786@gmail.com>
2021-02-15 21:32:55 +0530

    Fix cookie issue in local env in session_store initializers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_bookstore_app", domain: "https://mk-bookstore-backend.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_bookstore_app", domain: "https://mk-bookstore-backend.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_bookstore_app"
+end
