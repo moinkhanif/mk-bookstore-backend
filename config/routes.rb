e60f339174442295f650c3164613d37b9871d097
Moin Khan <kmoin786@gmail.com>
2021-02-16 16:44:06 +0530

    Change get suggest route to post

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
-  get :suggest, to: "books#suggest"
+  post :suggest, to: "books#suggest"
   root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
