d91b60851e5171723a8f222b840d4b38f3bb3d11
Moin Khan <kmoin786@gmail.com>
2021-02-16 14:47:40 +0530

    Add index and show method to Books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,28 @@
 class BooksController < ApplicationController
   def create
   end
+  def index
+    if @current_user
+      render json: {
+        books: @current_user.books
+      }
+    else
+      render json: {
+        status: "Please login to view",
+        books: user.books,
+        totalBooks: user.books.length
+      }
+    end
+  end
+  def show
+    if @current_user
+      render json: {
+        books: @current_user.books[params[:id].to_i]
+      }
+    else
+      render json: {
+        status: "Please login to view"
+      }
+    end
+  end
 end
